59b20632087223e9b259f6608f87c71217c46d19
Kovalchuk63 <vov4uk4763@gmail.com>
2023-11-27 12:08:29 +0200

    fix

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -9,7 +9,7 @@ class Component<T> {
   constructor(public props: T) {}
 }
 
-class Page extends Component<{ title: string }> {
+class Page extends Component<ComponentProps> {
   pageInfo() {
     console.log(this.props.title);
   }
